dbe3f738fb9564e2b5961be20c49178b8df647bb
Joao Leal <joaoo.lealc@gmail.com>
2024-02-14 15:15:02 -0300

    feat: Add support for SQLite and PostgreSQL databases

--- core/settings/databases.py
+++ core/settings/databases.py
@@ -1,17 +1,19 @@
 from decouple import config
 
-NAME_DB = config("NAME_DB", default="")
-USER_DB = config("USER_DB", default="")
-PASSWORD_DB = config("PASSWORD_DB", default="")
-HOST_DB = config("HOST_DB", default="")
+ENGINE_DB = config("DATABASE_ENGINE", default="django.db.backends.sqlite3")
+NAME_DB = config("DATABASE_NAME", default="")
+USER_DB = config("DATABASE_USER", default="")
+PASSWORD_DB = config("DATABASE_PASSWORD", default="")
+HOST_DB = config("DATABASE_HOST", default="")
+PORT_DB = config("DATABASE_PORTB", default="")
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": ENGINE_DB,
         "NAME": NAME_DB,
         "USER": USER_DB,
         "PASSWORD": PASSWORD_DB,
         "HOST": HOST_DB,
-        "PORT": "5432",
+        "PORT": PORT_DB,
     }
 }
