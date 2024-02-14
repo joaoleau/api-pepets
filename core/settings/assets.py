de9b845e964f89b0beeed653c59bd07e384da8ea
Joao Leal <joaoo.lealc@gmail.com>
2024-02-14 14:57:47 -0300

    chore: Improve project settings organization

--- core/settings/assets.py
+++
@@ -1,6 +0,0 @@
-from .environment import BASE_DIR
-import os
-
-STATIC_URL = "static/"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- core/settings/i18n.py
+++
@@ -1,7 +0,0 @@
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "America/Sao_Paulo"
-
-USE_I18N = True
-
-USE_TZ = True
