dc3a853f617d556f26945854bea611eb8b37d243
Joao Vitor Leal <joaoo.lealc@gmail.com>
2024-04-02 17:51:50 -0300

    refactor: Rename classes in managers.py file

--- posts/managers.py
+++ posts/managers.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.db.models.query import QuerySet
 
 
-class PostQuerySet(models.QuerySet):
+class PetQuerySet(models.QuerySet):
     def publish(self):
         return self.update(is_published=True)
 
 
-class PostManager(models.Manager):
+class PetsManager(models.Manager):
     def published(self):
         return self.filter(is_published=True)
 
     def get_queryset(self) -> QuerySet:
-        return PostQuerySet(self.model, using=self._db)
+        return PetQuerySet(self.model, using=self._db)
