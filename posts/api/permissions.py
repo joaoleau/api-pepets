3a181992f4da99ff7670315b8ca39f47892e415c
Joao Vitor Leal <joaoo.lealc@gmail.com>
2024-04-02 17:58:34 -0300

    feat: Add new permission 'IsAuthorView'

--- posts/api/permissions.py
+++ posts/api/permissions.py
@@ -1,8 +1,27 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from posts.models import Pet
 
 
 class IsAuthorOrIsAuthenticatedReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (obj.pet.owner == request.user and request.user.is_authenticated) or (
+        return (obj.owner == request.user and request.user.is_authenticated) or (
             request.user.is_authenticated and request.method in SAFE_METHODS
+        )
+
+
+class IsAuthorObj(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user and request.user.is_authenticated
+
+
+class IsAuthorView(BasePermission):
+
+    def has_permission(self, request, view):
+
+        if not request.user.is_authenticated:
+            return False
+
+        return (
+            Pet.objects.filter(owner=request.user).exists()
+            and request.user.is_authenticated
         )
