3fa3270fcf5549811ef1b59399e8ed916ec8b2e1
Joao Vitor Leal <joaoo.lealc@gmail.com>
2024-04-02 17:55:34 -0300

    refactor: Refactor existing views and add LastLocalUpdateView

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,16 +1,21 @@
 from rest_framework.generics import (
+    GenericAPIView,
     ListAPIView,
     CreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from .permissions import IsAuthorOrIsAuthenticatedReadOnly
+from rest_framework import mixins
+from .permissions import IsAuthorOrIsAuthenticatedReadOnly, IsAuthorView
 from rest_framework.permissions import (
     IsAuthenticated,
 )
-from ..models import Post
-from .serializers import PostListSerializer, PostCreateSerializer, PostDetailSerializer
-from rest_framework.response import Response
-from rest_framework import status
+from posts.models import Pet, Local
+from .serializers import (
+    PetListSerializer,
+    PetCreateSerializer,
+    PetDetailSerializer,
+    LocalSerializer,
+)
 from rest_framework import filters
 from rest_framework import pagination
 from django_filters.rest_framework import DjangoFilterBackend
@@ -20,75 +25,32 @@ class PostPagination(pagination.PageNumberPagination):
     page_size = 25
 
 
-class PostUserListView(ListAPIView):
+class MePetsView(ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = PostListSerializer
-    queryset = Post.objects.published()
+    serializer_class = PetListSerializer
 
     def get_queryset(self):
-        qs = self.queryset.filter(pet__owner__slug=self.kwargs.get("slug"))
-        qs = qs.select_related("pet__last_local", "pet__owner")
-        return qs
-
-    def get(self, request, slug, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+        return Pet.objects.filter(owner=self.request.user)
 
 
-class PostMeView(ListAPIView):
+class PetCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = PostListSerializer
-    queryset = Post.objects.published()
+    serializer_class = PetCreateSerializer
 
-    def get_queryset(self):
-        return Post.objects.filter(pet__owner__slug=self.request.user.slug)
-
-
-class PostCreateView(CreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostCreateSerializer
-    queryset = Post.objects.published()
-
-    def post(self, request, *args, **kwargs):
-
-        if request.user.is_staff and request.data["pet"].get("owner", None) is not None:
-            return self.create(request, *args, **kwargs)
-        request.data["pet"].update({"owner": request.user.id})
-        return self.create(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class PostDetailView(RetrieveUpdateDestroyAPIView):
+class PetDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthorOrIsAuthenticatedReadOnly]
-    serializer_class = PostDetailSerializer
-    queryset = Post.objects.published()
+    serializer_class = PetDetailSerializer
+    queryset = Pet.objects.all()
     lookup_field = "slug"
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop("partial", False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
 
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
-    def get_serializer(self, *args, **kwargs):
-        serializer_class = self.get_serializer_class()
-        kwargs.setdefault("context", self.get_serializer_context())
-        return serializer_class(*args, **kwargs)
-
-
-class PostListView(ListAPIView):
-    serializer_class = PostListSerializer
-    queryset = Post
+class PetListView(ListAPIView):
+    serializer_class = PetListSerializer
+    queryset = Pet.objects.published()
     pagination_class = PostPagination
     filter_backends = [
         DjangoFilterBackend,
@@ -96,16 +58,14 @@ class PostListView(ListAPIView):
         filters.OrderingFilter,
     ]
     filterset_fields = [
-        "pet__status",
-        "pet__type",
-        "pet__last_local__street",
-        "pet__last_local__neighborhood",
-        "pet__last_local__city",
+        "status",
+        "type",
+        "local__street",
+        "local__neighborhood",
+        "local__city",
     ]
     search_fields = [
-        "pet__description",
         "description",
-        "title",
     ]
     ordering_fields = ["created_at"]
 
@@ -115,5 +75,20 @@ class PostListView(ListAPIView):
     def get_queryset(self):
         qs = super().get_queryset()
         qs = qs.objects.published()
-        qs = qs.select_related("pet", "pet__last_local", "pet__owner")
+        qs = qs.select_related("local", "owner")
         return qs
+
+
+class LastLocalUpdateView(mixins.UpdateModelMixin, GenericAPIView):
+    permission_classes = [IsAuthorView]
+    model = Local
+    serializer_class = LocalSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def get_object(self):
+        return Local.objects.get(pet__slug=self.kwargs.get("slug"))
